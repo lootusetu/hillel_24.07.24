37b37d035afb3f0e2db53caf883c27f8a8e5328a
lootusetu <tania.korota@gmail.com>
2024-08-01 19:04:39 +0300

    Second Commit

--- lesson_2.py
+++ lesson_2.py
@@ -1,3 +1,4 @@
+#HT №1
 number = input("Enter a 4-digit number: ")
 number = int(number)
 
@@ -11,3 +12,16 @@ print(first_number)
 print(second_number)
 print(third_number)
 print(fourth_number)
+
+#HT №2
+number_2 = int(input("Enter a 5-digit number: "))
+number_2 = int(number_2)
+
+number1 = number_2 // 10000
+number2 = (number_2 % 10000) // 1000
+number3 = (number_2 % 1000) // 100
+number4 = (number_2 % 100) // 10
+number5 = number_2 % 10
+
+result = number5 * 10000 + number4 * 1000 + number3 * 100 + number2 * 10 + number1
+print(result)
