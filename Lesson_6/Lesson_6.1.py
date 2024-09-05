5fe9aec972b0ee8df858d3b18978f12e4f8145c8
lootusetu <tania.korota@gmail.com>
2024-09-06 01:11:01 +0300

    3 Commit

--- Lesson_6/Lesson_6.1.py
+++ Lesson_6/Lesson_6.1.py
@@ -1,9 +1,9 @@
 import  string
-input data = string.ascii_letters
+input_data = string.ascii_letters
 
 vvodnie_danie = input('введите диапазон букв \"пример: а-b\"')
 
 start_letter, end_letter = vvodnie_danie.split('-')
-start_letter = nabor_bukv.index(start_letter)
-end_letter = nabor_bukv.index(end_letter)
-print(nabor_bukv[start_letter:end_letter + 1])
+start_letter = input_data.index(start_letter)
+end_letter = input_data.index(end_letter)
+print(input_data[start_letter:end_letter + 1])
